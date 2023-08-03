0ec5cf74f5948761174ba51438760c2125e4b9ff
PRATIK PAWAR <85684773+Pawar-Pratik@users.noreply.github.com>
2023-08-03 23:53:19 +0530

    Create 9. Palindrome Number.cpp

---
+++ 9. Palindrome Number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+			if( x<0 ){
+				return false;
+			}
+			long long r=0;
+			long long temp=x;
+			while(x!=0){
+				int digit = x%10;
+				r = (r*10)+digit;
+				x /= 10;
+			}
+			if(r==temp){
+				return true;
+			}
+			else{
+				return false;
+			}
+		}
+};
